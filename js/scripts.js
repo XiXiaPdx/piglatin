b9b6d9c7c799e564591ac140766105ceb64fbf9d
Ben Schenkenberger and Xi Xia <ben+xi@>
2017-02-22 14:44:26 -0800

    add check first letter for vowel and add AY to end of word

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,38 @@
 var latinOutput;
 var englishInput;
 var vowels = ["a","e","i","o","u","A","E","I","O","U"];
+var inputArray = [];
+
 //Behavior1: A year that is NOT a leap year
-var translateLetter = function (letter) {
+var makeIndCharacterArray = function (word) {
+  inputArray=word.split("");
+}
+
+var rejoinCharacterArray = function (latinArray) {
+  latinOutput=latinArray.join("");
+}
+
+
+var translateString = function (inputString) {
+  for (j=0;j<inputString.length;j++)
+  if (checkLetterIsVowel(inputString[0])){
+     inputArray.push("ay");
+     console.log(inputArray);
+     return;
+  } else {
+
+  };
+};
+
+var checkLetterIsVowel = function (letter) {
   for (i=0;i<vowels.length;i++) {
-      if (letter === vowels[i]) {
-      latinOutput = letter+"ay"
-      return;
+      if (letter[j] === vowels[i]) {
+      return true;
     }
     else {
       latinOutput = letter;
     }
-  }
+  };
 };
 
 //user interface logic:
@@ -21,8 +42,9 @@ $(document).ready(function() {
   $("form#englishForm").submit(function(event) {
       event.preventDefault();
       var englishInput = $("input#english").val();
-      translateLetter (englishInput);
-
+      makeIndCharacterArray (englishInput);
+      translateString (inputArray);
+      rejoinCharacterArray (inputArray);
       $("#result").text(latinOutput);
       $("#result").show();
   });
