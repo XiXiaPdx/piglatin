e0c1f2d841a93f067fbb54a4aa4ddeae91ec2d53
Ben Schenkenberger and Xi Xia <ben@>
2017-02-22 10:03:32 -0800

    Pass first spec: user input is received, parsed, and passed through leapYear function and returns false from the called function as expected.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 
-$(document).ready(function (){
+//business logic:
 
+//Behavior1: A year that is NOT a leap year
+var leapYear = function(year) {
+  return false;
+};
+
+//user interface (front end) logic:
+$(document).ready(function (){
+  $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+      $("#result").text(result);
+  });
 });
