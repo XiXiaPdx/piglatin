89131cbdbf7a42b91c30cb159324731b29aaf586
Ben Schenkenberger and Xi Xia <ben+xi@>
2017-02-22 16:53:19 -0800

    add period checking at end of world

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,59 @@
 
 //business logic:
-var latinOutput;
+var latinOutput="";
 var englishInput;
 var vowels = ["a","e","i","o","u","A","E","I","O","U"];
 var inputArray = [];
+var masterArray =[];
+var endSymbol = []; // check word, if symbol at end, make endSymbol = that symbol. And chop off that symbol from the word. Run our program as usual.
+
+var makeIndWordArrays = function(words){
+  masterArray=words.split(" ");
+}
 
 //Behavior1: A year that is NOT a leap year
 var makeIndCharacterArray = function (word) {
   inputArray=word.split("");
+    if(inputArray[inputArray.length - 1] ==="."){
+      endSymbol[0]=".";
+      inputArray.splice((inputArray.length-1),1);
+    } else {
+      endSymbol[0]="";
+    }
+    console.log(endSymbol);
+    console.log(inputArray);
 }
 
 var rejoinCharacterArray = function (latinArray) {
-  latinOutput=latinArray.join("");
+  latinOutput=latinOutput+latinArray.join("")+endSymbol[0]+" ";
 }
 
 
 var translateString = function (inputString) {
-  for (j=0;j<inputString.length;j++) {
+  for (var j=0;j<inputString.length;j++) {
     if (checkLetterIsVowel(inputString[0])){
        inputArray.push("ay");
        return;
     } else if
         ((inputString[0] === "q" || inputString[0] === "Q") && (inputString[1] === "u" || inputString[1] ==="U")) {
-                inputArray.push(inputString[0]);
-                inputArray.push(inputString[1]);
-                inputArray.push("ay");
-                inputArray.splice(0,2);      
-                return;
-              }
-                else {
-                var consonant = inputString[0];
-                inputArray.push(consonant);
-                inputArray.splice(0,1);
-                console.log(inputArray);
+              inputArray.push(inputString[0]);
+              inputArray.push(inputString[1]);
+              inputArray.push("ay");
+              inputArray.splice(0,2);
+              return;
+            }
+              else {
+              var consonant = inputString[0];
+              inputArray.push(consonant);
+              inputArray.splice(0,1);
+              console.log(inputArray);
               };
         };
   };
 
 
 var checkLetterIsVowel = function (letter) {
-  for (i=0;i<vowels.length;i++) {
+  for (var i=0;i<vowels.length;i++) {
       if (letter === vowels[i]) {
       return true;
     }
@@ -48,15 +62,19 @@ var checkLetterIsVowel = function (letter) {
     }
   };
 };
-
 //user interface logic:
 $(document).ready(function() {
   $("form#englishForm").submit(function(event) {
       event.preventDefault();
       var englishInput = $("input#english").val();
-      makeIndCharacterArray (englishInput);
+      makeIndWordArrays (englishInput);
+
+      for (var b=0;b<masterArray.length;b++) {
+      makeIndCharacterArray (masterArray[b]);
       translateString (inputArray);
       rejoinCharacterArray (inputArray);
+      };
+
       $("#result").text(latinOutput);
       $("#result").show();
   });
